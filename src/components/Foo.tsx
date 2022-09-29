7e944d39d1d062e7927abb7035383dfb34a7588f
clairefro <claire.froelich@gmail.com>
2022-09-28 21:07:14 -0400

    Move all source files to src

---
+++ src/components/Foo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Foo = (props: Props) => {
+  return <div>does this work?</div>;
+};
+
+export default Foo;
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import Foo from "../components/Foo";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      HOME
+      <Foo />
+    </div>
+  );
+};
+
+export default Home;
