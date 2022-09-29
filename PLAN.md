752c00b1339625ad0bd0fd3b2f6cc633975fd9a8
clairefro <claire.froelich@gmail.com>
2022-09-28 21:01:21 -0400

    Houston we have contact

---
+++ PLAN.md
@@ -0,0 +1,13 @@
+### pages
+
+- home
+- puppies avail
+- studs avail
+- gallery
+- sales policy
+- contract
+
+#### templates
+
+- puppy details
+- stud details
---
+++ pages/contact/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+type Props = {};
+
+const Contact: NextPage = (props: Props) => {
+  return <div>contact</div>;
+};
+
+export default Contact;
