dac81cd13ecf85b88f70c5c93c6cdeb6c6844e8c
clairefro <claire.froelich@gmail.com>
2022-10-06 21:36:42 -0400

    Clean homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import SvgIcon from "../components/blocks/icons/SvgIcon";
 import PaddedContainer from "../components/containers/PaddedContainer";
 import SpacerSm from "../components/spacers/SpacerSm";
 
@@ -17,16 +16,6 @@ const Home: NextPage = () => {
         foofoo foofoo foofoo foofoo foofoo foofoo foo
       </PaddedContainer>
       <SpacerSm />
-      <SvgIcon title="Read more" size="lg" src="/assets/icons/angle-down.svg" />
-      {Array.apply(null, Array(200))
-        .map(function () {})
-        .map((i, j) => {
-          return (
-            <p key={j}>
-              HOMEHOMEHOMEHOMEHOMEHOMEHOMEHOMEHOMEHOMEHOMEHOMEHOMEHOMEHOMEHOMEHOMEHOMEHOMEHOMEHOMEHOME
-            </p>
-          );
-        })}
     </div>
   );
 };
