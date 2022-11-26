dacd11528995cd72b05deb172acbdac9b9e844ff
clairefro <claire.froelich@gmail.com>
2022-11-26 16:30:06 -0500

    Migrate to next/fonts instead of loading local fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,20 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import GlobalLayout from "../layouts/GlobalLayout";
 
+/** Load global font on build. Applied as class on <main> */
+import { Inria_Sans } from "@next/font/google";
+const inriaSans = Inria_Sans({
+  weight: ["300", "400", "700"],
+  subsets: ["latin"],
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <GlobalLayout>
-      <Component {...pageProps} />
-    </GlobalLayout>
+    <main className={inriaSans.className}>
+      <GlobalLayout>
+        <Component {...pageProps} />
+      </GlobalLayout>
+    </main>
   );
 }
 
