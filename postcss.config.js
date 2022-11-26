779ec49509fa395a3d211f895d67f959571625bc
clairefro <claire.froelich@gmail.com>
2022-11-26 11:49:20 -0500

    Trying to sync tailwind congif with postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -5,10 +5,7 @@ module.exports = {
       ? [
           "@fullhuman/postcss-purgecss",
           {
-            content: [
-              "./pages/**/*.{js,jsx,ts,tsx}",
-              "./components/**/*.{js,jsx,ts,tsx}",
-            ],
+            content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
             defaultExtractor: (content) =>
               content.match(/[\w-/:]+(?<!:)/g) || [],
           },
