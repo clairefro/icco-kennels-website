cbda81612bab03eb8ff448982ff16826a62e364c
clairefro <claire.froelich@gmail.com>
2022-11-26 11:34:47 -0500

    Trying patch for tailwind 3.1 deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
   //   "./src/components/**/*.{js,ts,jsx,tsx}",
   //   "./src/pages/**/*.{js,ts,jsx,tsx}",
   // ],
-  important: true,
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
